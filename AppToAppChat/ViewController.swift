1d4c3bb2b952667961f99621c05d090daeb6feb6
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2020-07-30 17:36:47 +0100

    add tutorial code

--- AppToAppChat/ViewController.swift
+++ AppToAppChat/ViewController.swift
@@ -7,14 +7,114 @@
 //
 
 import UIKit
+import NexmoClient
 
 class ViewController: UIViewController {
+    
+    let loginAliceButton = UIButton(type: .system)
+    let loginBobButton = UIButton(type: .system)
+    let statusLabel = UILabel()
+    
+    let client = NXMClient.shared
+    
+    var user: User? {
+        didSet {
+            login()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        loginAliceButton.setTitle("Log in as Alice", for: .normal)
+        loginAliceButton.translatesAutoresizingMaskIntoConstraints = false
+        loginAliceButton.addTarget(self, action: #selector(setUserAsAlice), for: .touchUpInside)
+        view.addSubview(loginAliceButton)
+        
+        loginBobButton.setTitle("Log in as Bob", for: .normal)
+        loginBobButton.translatesAutoresizingMaskIntoConstraints = false
+        loginBobButton.addTarget(self, action: #selector(setUserAsBob), for: .touchUpInside)
+        view.addSubview(loginBobButton)
+        
+        statusLabel.text = ""
+        statusLabel.textAlignment = .center
+        statusLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(statusLabel)
+        
+        NSLayoutConstraint.activate([
+            loginAliceButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginAliceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginAliceButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            loginAliceButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            loginBobButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginBobButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            loginBobButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            loginBobButton.topAnchor.constraint(equalTo: loginAliceButton.bottomAnchor, constant: 20),
+            
+            statusLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            statusLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            statusLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            statusLabel.topAnchor.constraint(equalTo: loginBobButton.bottomAnchor, constant: 20)
+        ])
     }
 
+    @objc func setUserAsAlice() {
+        self.user = User.Alice
+    }
+    
+    @objc func setUserAsBob() {
+        self.user = User.Bob
+    }
+    
+    func login() {
+        guard let user = self.user else { return }
+        
+        client.setDelegate(self)
+        client.login(withAuthToken: user.jwt)
+    }
 
+}
+
+extension ViewController: NXMClientDelegate {
+    func client(_ client: NXMClient, didChange status: NXMConnectionStatus, reason: NXMConnectionStatusReason) {
+        guard let user = self.user else { return }
+        
+        switch status {
+        case .connected:
+            self.statusLabel.text = "Connected"
+            let navigationController = UINavigationController(rootViewController: ChatViewController(user: user))
+            navigationController.modalPresentationStyle = .overFullScreen
+            present(navigationController, animated: true, completion: nil)
+        case .disconnected:
+            self.statusLabel.text = "Disconnected"
+        case .connecting:
+            self.statusLabel.text = "Connecting"
+        @unknown default:
+            self.statusLabel.text = ""
+        }
+    }
+    
+    func client(_ client: NXMClient, didReceiveError error: Error) {
+        self.statusLabel.text = error.localizedDescription
+    }
+}
+
+struct User {
+    let name: String
+    let uuid: String
+    let jwt: String
+    let chatPartnerName: String
+    let conversationId = "CONVERSATION_ID"
+    
+    static let Alice = User(name: "Alice",
+                            uuid: "ALICE_USERID",
+                            jwt:"ALICE_JWT",
+                            chatPartnerName: "Bob")
+    static let Bob = User(name: "Bob",
+                          uuid: "BOB_USERID",
+                          jwt:"BOB_JWT",
+                          chatPartnerName: "Alice")
 }
 
---
+++ Pods/Target Support Files/Pods-AppToAppChat/Pods-AppToAppChat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppToAppChat : NSObject
+@end
+@implementation PodsDummy_Pods_AppToAppChat
+@end
